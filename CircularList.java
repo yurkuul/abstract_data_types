9f10d873d5b30b4930eaba69c1cd65242f007bd4
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2023-07-13 16:23:01 -0500

    Update CircularList.java

--- CircularList.java
+++ CircularList.java
@@ -24,3 +24,25 @@ public class CircularList {
 
     }
 }
+
+class Node {
+    private int data;
+    private Node next;
+
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
