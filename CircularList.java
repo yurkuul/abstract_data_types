8256ea880d845abb8c65f5531c19426f81dd6b33
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2023-07-18 20:12:42 -0500

    Update CircularList.java

--- CircularList.java
+++ CircularList.java
@@ -25,6 +25,84 @@ public class CircularList {
     }
 }
 
+class CircularLinkedList {
+    private Node first;
+    private int size;
+
+    public CircularLinkedList() {
+        first = null;
+        size = 0;
+    }
+    public void add(int value) {
+        if (first == null) {
+            first = new Node(value, null);
+            first.setNext(first);
+        } else {
+            Node newNode = new Node(value, first.getNext());
+            first.setNext(newNode);
+        }
+        size++;
+    }
+
+    public int get() {
+        return first.getData();
+    }
+
+    public void rotate() {
+        if (first != null)
+            first = first.getNext();
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public int count(int value) {
+        int count = 0;
+        for (int i = 0; i < size(); i++) {
+            if (get() == value)
+                count++;
+            rotate();
+        }
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        String print = "The list contains: ";
+        if (size > 0) {
+            Node currNode = first;
+            for (int i = 0; i < size - 1; i++) {
+                print += currNode.getData() + ", ";
+                currNode = currNode.getNext();
+            }
+            print += currNode.getData() + ".";
+        } else {
+            print += "Nothing!  The list is empty.";
+        }
+        return print;
+    }
+
+    public int remove() {
+        if (size > 0) {
+            Node oldFirst = first;
+    
+            Node end = first;
+            for (int i = 0; i < size - 1; i++) { 
+                end = end.getNext();
+            }
+    
+            end.setNext(first.getNext());
+            first = first.getNext();
+            oldFirst.setNext(null);
+            size--; 
+    
+            return oldFirst.getData();
+        }
+        return Integer.MIN_VALUE;
+    }
+}
+
 class Node {
     private int data;
     private Node next;
