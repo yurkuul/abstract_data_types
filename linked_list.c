ce695639e883024c9867a36db0551701bd0bb0a2
lisazhu.dev <95185447+yurkuul@users.noreply.github.com>
2023-10-30 16:57:15 -0500

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -1,15 +1,38 @@
+/**
+ * @file
+ * @brief Linked list implementation.
+ * 
+ * File contains a Node struct for a Linked List struct and includes standard
+ * functions for creation of a linked list, adding Nodes to the Linked List,
+ * removing Nodes, and displaying contents of the Linked List.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * @struct Node
+ * @brief Represents a node in a linked list.
+*/
 typedef struct Node {
     int data;
     struct Node* next;
 } Node;
 
+/**
+ * @struct LinkedList
+ * @brief Represents a linked list.
+*/
 typedef struct LinkedList {
     Node* head;
 } LinkedList;
 
+/**
+ * @brief Creates a new node with the given data.
+ * 
+ * @param data The data to be stored in the new node.
+ * @return A pointer to the newly created node.
+*/
 Node* createNode(int data) {
     Node* newNode = (Node*)malloc(sizeof(Node));
     newNode->data = data;
@@ -17,12 +40,23 @@ Node* createNode(int data) {
     return newNode;
 }
 
+/**
+ * @brief Creates a new empty linked link.
+ * 
+ * @return A pointer to the newly created linked list.
+*/
 LinkedList* createLinkedList() {
     LinkedList* list = (LinkedList*)malloc(sizeof(LinkedList));
     list->head = NULL;
     return list;
 }
 
+/**
+ * @brief Adds a new node with the given data to the end of the linked list.
+ *
+ * @param list A pointer to the linked list.
+ * @param data The data to be added to the list.
+ */
 void add(LinkedList* list, int data) {
     Node* newNode = createNode(data);
     if (list->head == NULL) {
@@ -36,6 +70,13 @@ void add(LinkedList* list, int data) {
     }
 }
 
+/**
+ * @brief Removes the first occurrence of a node with the given data from the
+ * linked list.
+ *
+ * @param list A pointer to the linked list.
+ * @param data The data to be removed from the list.
+ */
 void removeElement(LinkedList* list, int data) {
     Node* current = list->head;
     Node* prev = NULL;
@@ -59,6 +100,11 @@ void removeElement(LinkedList* list, int data) {
     free(current);
 }
 
+/**
+ * @brief Displays the elements of the linked list.
+ *
+ * @param list A pointer to the linked list.
+ */
 void display(LinkedList* list) {
     Node* current = list->head;
     while (current != NULL) {
@@ -66,4 +112,34 @@ void display(LinkedList* list) {
         current = current->next;
     }
     printf("NULL\n");
+}
+
+int main() {
+    LinkedList* list = createLinkedList();
+
+    add(list, 1);
+    add(list, 2);
+    add(list, 3);
+    add(list, 4);
+    add(list, 5);
+
+    printf("Linked List: ");
+    display(list);
+
+    removeElement(list, 3);
+
+    printf("After removing 3: ");
+    display(list);
+
+    removeElement(list, 1);
+
+    printf("After removing 1: ");
+    display(list);
+
+    removeElement(list, 5);
+
+    printf("After removing 5: ");
+    display(list);
+
+    return 0;
 }
